845a29eaf14366c2cc0c00925a4e9a55d9c76fb3
Lucas F <franco100lucas@gmail.com>
2025-09-17 05:44:35 -0300

    Feat: Add user and post relationship, and create migration to incluse user_id on posts table

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     protected $fillable = ['title', 'author', 'excerpt', 'text'];
+
+    protected function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
